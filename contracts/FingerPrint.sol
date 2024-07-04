b2428fe1c2f196162711b8cb2a0a57696e96785f
eludius18 <eladio.robcas@gmail.com>
2024-07-04 11:39:00 +0100

    initial setup

---
+++ contracts/FingerPrint.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./libraries/MMR.sol";
+import "./interfaces/IFingerPrint.sol";
+
+/*
+
+                 .-+*###+-.
+               =*%%%%%%%%%%#=:
+               :=*%%%%%%%%%%%#+-.
+                   .-+#%%%%%%%%%%%*=:
+    :+##+-             :=#%%%%%%%%%%%#+-
+   *%%%%%%%*=:            .-+#%%%%%%%%%%*.
+  *%%%%%%%%#+:                :=#%%%%%%%%*
+  #%%%%%%*:         .==:         .*%%%%%%%
+  #%%%%%%=       :+#%%%%#+-       -%%%%#+:
+  #%%%%%%=     :#%%%%%%%%%%#-     -%*=.
+  #%%%%%%=     -%%%%%%%%%%%%=     .
+  #%%%%%%=     -%%%%%%%%%%%%=
+  #%%%%%%=     -%%%%%%%%%%%%=            :
+  #%%%%%%=      .=*%%%%%%*=:         .-+#%
+  #%%%%%%=          -++-.         :=#%%%%%
+  *%%%%%%=                    .-+#%%%%%%%#
+  .#%%%%%=                 :=*%%%%%%%%%%#:
+    =*%%%=       #+-.  .-+#%%%%%%%%%%%*=
+       -+=       #%%%##%%%%%%%%%%%#*-.
+                 #%%%%%%%%%%%%%#=:
+                 #%%%%%%%%%#*-.
+                 :=*%%%%#=:
+
+*/
+
+/// @title FingerPrint Contract using Mountain Merkle Range Tree
+/// @author eludius18
+/// @notice This contract allows for the storage and retrieval of fingerprints (hashes)
+/// @dev The contract uses keccak256 to generate the digital fingerprints of the entered data.
+contract FingerPrint is 
+Initializable,
+AccessControlUpgradeable,
+MMR,
+IFingerPrint
+{
+  using Strings for string;
+
+  bytes32 public constant ADMIN_ROLE = keccak256("ADMIN");
+
+}
